6834002e3ffc50a0e2bbdf7d616176b2ea83bacf
JinseoKim <eunoia@handong.ac.kr>
2023-07-17 12:58:01 +0900

    Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,1 +1,15 @@
-export default () => <span>Profile</span>;
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
+
+export default () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
